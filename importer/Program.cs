de42cff3fde3eb10a753687ef4276472a10b7b42
Claude Glauser <public@claude-glauser.ch>
2020-12-12 19:24:12 +0100

    feat: add configuration and user secrets.

--- importer/Program.cs
+++ importer/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OpenSportsPlatform.Lib.DependencyInjection;
 using OpenSportsPlatform.Lib.Services.Contract;
+using System;
+using System.IO;
 
 namespace OpenSportsPlatform.Importer
 {
@@ -19,6 +22,14 @@ namespace OpenSportsPlatform.Importer
             ILogger logger = serviceProvider.GetService<ILoggerFactory>()
                 .CreateLogger<Program>();
             logger.LogDebug("Starting importer");
+
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .AddUserSecrets<Program>()
+                .Build();
+
 
             IJsonFileImporterService jsonFileImporterService = serviceProvider.GetService<IJsonFileImporterService>();
             jsonFileImporterService.ImportFiles();
