464db858511a86dc4a7eedfaf15dd5396696e7a5
Claude Glauser <public@claude-glauser.ch>
2021-01-03 17:06:23 +0100

    feat: added some stuff.

--- application/ClientApp/src/app/app-routing.module.ts
+++ application/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { WorkoutOverViewComponent } from './workout-over-view/workout-over-view.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/workouts', pathMatch: 'full' },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
   { path: 'workouts', component: WorkoutOverViewComponent },
 ];
 
