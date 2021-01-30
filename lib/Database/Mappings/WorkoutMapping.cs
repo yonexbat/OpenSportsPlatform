41b7de652ce144b75d5f9b3413adf4730b69bf66
yonexbat <yonexbat@gmail.com>
2021-01-30 14:48:14 +0100

    fix: Changed case of a character in directory. Problem on linux, not on windows.

---
+++ lib/Database/Mappings/WorkoutMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenSportsPlatform.Lib.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSportsPlatform.Lib.Database.Mappings
+{
+    public class WorkoutMapping : IEntityTypeConfiguration<Workout>
+    {
+        public void Configure(EntityTypeBuilder<Workout> builder)
+        {
+            builder.ToTable("OSPWorkout");
+
+            builder.HasOne(wo => wo.SportsCategory)
+                .WithMany(cat => cat.Workouts)
+                .HasForeignKey(c => c.SportsCategoryId);
+
+            builder.HasOne(wo => wo.UserProfile)
+                .WithMany(cat => cat.Workouts)
+                .HasForeignKey(c => c.UserProfileId);
+        }
+    }
+}
