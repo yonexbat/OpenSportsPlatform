a48eb95eddcec7de59d8c085ce8d76190394f06b
Claude Glauser <public@claude-glauser.ch>
2021-10-24 09:15:36 +0200

    some improvements

--- application/ClientApp/src/app/sync/sync.component.ts
+++ application/ClientApp/src/app/sync/sync.component.ts
@@ -9,14 +9,21 @@ import { DataService } from '../data.service';
 })
 export class SyncComponent implements OnInit {
 
+  public showSpinner = false;
+
   constructor(private dataService: DataService, private confirmService: ConfirmService) { }
 
   ngOnInit(): void {
   }
 
   public async syncPolar(): Promise<void> {
-    await this.dataService.syncPolar();
-    await this.confirmService.inform('synchronize', 'synchronization is done');
+    try {
+      this.showSpinner = true;
+      await this.dataService.syncPolar();
+      await this.confirmService.inform('synchronize', 'synchronization is done');
+    } finally {
+      this.showSpinner = false;
+    }
   }
 
 }
