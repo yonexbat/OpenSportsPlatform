3bc9127e2867434033e685dcb3dd97486c1af750
Claude Glauser <public@claude-glauser.ch>
2022-12-30 15:42:14 +0100

    correction in paging.

--- application/ClientApp/src/app/workout-over-view2/workout-over-view2.component.ts
+++ application/ClientApp/src/app/workout-over-view2/workout-over-view2.component.ts
@@ -21,17 +21,18 @@ export class WorkoutOverView2Component implements OnInit {
 
   @ViewChild('ScrollerRef', { static: true }) scroller!: CdkVirtualScrollViewport;
 
-  constructor(private dataService: DataService, private ngZone: NgZone) {
-    this.fetchMore();
+  constructor(private dataService: DataService, private ngZone: NgZone) {    
   }
 
   ngOnInit(): void {
+    this.onInitAsync();
+  }
 
+  private async onInitAsync(): Promise<void> {
     const height = this.scroller.measureViewportSize('vertical');
     console.log(`viewport size is ${height}`);
-    const numPages = height / (this.workoutHeightPx * 10);
-    this.fetchPages(numPages);
-
+    const numPages = height / (this.workoutHeightPx * 10);    
+    await this.fetchPages(numPages > 0 ? numPages : 1);
     
     this.scroller.elementScrolled()
     .pipe(
@@ -46,17 +47,17 @@ export class WorkoutOverView2Component implements OnInit {
     });
   }
 
-  private async fetchPages(numLoads: number): Promise<void> {
+  private async fetchPages(numLoads: number): Promise<void> {    
     while(numLoads > 0) {
       await this.fetchMore();
       numLoads --;
     }
   }
 
-  public async fetchMore(): Promise<void> {
-    this.currentPage++;
-    const newItems = await this.dataService.searchWorkoutItems(this.currentPage - 1);
-    this.workoutItems = [...this.workoutItems, ...newItems.data];
+  public async fetchMore(): Promise<void> {   
+    const newItems = await this.dataService.searchWorkoutItems(this.currentPage);
+    this.workoutItems = [...this.workoutItems, ...newItems.data]; 
+    this.currentPage++;   
   }
 
   public getImage(sportCat: string): string {
